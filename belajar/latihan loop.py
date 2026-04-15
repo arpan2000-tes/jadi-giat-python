c15d118f2984cf4f4301db923d00bee56e79df91
arpan2000-tes <marfansmp02n@gmail.com>
2026-04-16 03:00:18 +0800

    tes

--- belajar/latihan loop.py
+++ belajar/latihan loop.py
@@ -1,4 +1,4 @@
-sisi = 5
+sisi = 10
 panjang = 1
 sisi2 = 0
 panjang2 = 5
@@ -22,4 +22,28 @@ for i in range(n *2) :
             print ("*", end= "")
         else :
             print (".", end="")
+    print()
+    
+print("\n\n")
+
+sisi = 10
+panjang =1
+
+spasi = int(sisi/2)
+while True:
+    if (panjang%2) :
+        print(" "*spasi, "*"*panjang)
+        spasi -= 1
+        panjang +=1
+    else :
+        panjang += 1
+        continue
+    
+    if panjang > sisi:
+        break
+    
+for i in range (5,0,-1) :
+    print(""*(5-i), end="")
+    for x in range(i):
+        print("*", end=" ")
     print()
