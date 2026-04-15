985733e40ee02a8fa92dbad85e5a8d9851f86485
arpan2000-tes <marfansmp02n@gmail.com>
2026-04-16 01:55:18 +0800

    New project

---
+++ latihan loop.py
@@ -0,0 +1,6 @@
+sisi = 5
+
+nomor = 1
+for i in range(sisi):
+	print ("*" * nomor )
+	nomor += 1
---
+++ newfile.py
@@ -0,0 +1,10 @@
+angka = 0
+
+while angka <= 1000:
+	angka += 1 
+	
+	if angka >= 500 :
+		break 
+	
+
+	print (angka )
