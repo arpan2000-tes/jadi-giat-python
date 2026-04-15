5886b5830aa6ebcfdfd3be599731c88c5ee8d959
arpan2000-tes <marfansmp02n@gmail.com>
2026-04-16 02:08:57 +0800

    tes

--- belajar/latihan loop.py
+++ belajar/latihan loop.py
@@ -1,6 +1,13 @@
 sisi = 5
+panjang = 1
+sisi2 = 0
+panjang2 = 5
 
-nomor = 1
-for i in range(sisi):
-	print ("*" * nomor )
-	nomor += 1
+    
+for i in range(sisi) :
+    print("*"* panjang)
+    panjang += 1
+    
+for i in range(sisi) :
+    print("*"* panjang)
+    panjang -= 1 
