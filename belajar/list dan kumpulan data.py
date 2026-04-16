48a7b1dc009e31a2f660d848f32fbfcaea445cb0
arpan2000-tes <marfansmp02n@gmail.com>
2026-04-16 21:03:55 +0800

    tes

---
+++ belajar/list dan kumpulan data.py
@@ -0,0 +1,10 @@
+data_campuran = [1,"tambah",True,2,"kurang",False]
+print(f"semua data campuran {data_campuran}")
+
+data_range = range(0,90,2)
+print (data_range)
+data_list = list(data_range)
+print (data_list)
+
+list_pake_for = [i**10 for i in range(0,10)]
+print(list_pake_for)
